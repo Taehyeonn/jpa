fb18468e559fbe88b8a1456aaf1b4bc9298fd1f1
taehyeon <97taehyun@gmail.com>
2024-02-03 23:08:16 +0900

    페이징 기능 구현 (#14)

--- src/main/java/com/example/jpa/post/service/PostService.java
+++ src/main/java/com/example/jpa/post/service/PostService.java
@@ -7,6 +7,10 @@ import com.example.jpa.post.dto.response.PostResponseDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -71,5 +75,19 @@ public class PostService {
 
         post.ifPresent(value -> value.update(postRequestDto.getTitle(),
                 postRequestDto.getContent()));
+    }
+
+    public Page<PostResponseDto> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() - 1; // page 위치에 있는 값은 0부터 시작한다.
+        int pageLimit = 3; // 한페이지에 보여줄 글 개수
+
+        // 한 페이지당 3개식 글을 보여주고 정렬 기준은 ID기준으로 내림차순
+        Page<Post> postsPages = postRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+        // 목록 : id, title, content, author
+        Page<PostResponseDto> postsResponseDtos = postsPages.map(
+                postPage -> new PostResponseDto(postPage));
+
+        return postsResponseDtos;
     }
 }
